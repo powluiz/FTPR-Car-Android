5b06988a3a1f8149ffa3c0852ec3f14e5d17d366
Luiz Ricardo Brumati De Lima <luiz.1303@hotmail.com>
2025-09-04 23:07:11 -0300

    feat: add picture upload with device camera

--- app/src/main/java/com/example/myapitest/NewVehicleActivity.kt
+++ app/src/main/java/com/example/myapitest/NewVehicleActivity.kt
@@ -1,13 +1,23 @@
 package com.example.myapitest
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.View
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat.checkSelfPermission
+import androidx.core.content.FileProvider
+import androidx.core.content.PermissionChecker.PERMISSION_GRANTED
 import androidx.core.widget.addTextChangedListener
 import com.example.myapitest.databinding.ActivityNewVehicleBinding
 import com.example.myapitest.model.Location
@@ -24,6 +34,9 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
 import java.util.UUID
 
 class NewVehicleActivity : AppCompatActivity() {
@@ -31,8 +44,22 @@ class NewVehicleActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNewVehicleBinding
     private lateinit var mapManager: MapManager
     private lateinit var locationPermissionLauncher: ActivityResultLauncher<String>
-
+    private lateinit var imageUri: Uri
     private var selectedMarker: Marker? = null
+    private var imageFile: File? = null
+    private var uploadedImageUrl: String? = null
+
+    private val cameraLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == Activity.RESULT_OK) {
+            imageFile?.let {
+                binding.image.setImageBitmap(BitmapFactory.decodeFile(it.path))
+                uploadedImageUrl = it.path
+            }
+//            uploadImageToFirebase()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +69,23 @@ class NewVehicleActivity : AppCompatActivity() {
 
         setupManagers()
         setupView()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_CODE_CAMERA -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PERMISSION_GRANTED) {
+                    openCamera()
+                } else {
+                    Toast.makeText(this, R.string.error_request_camera, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 
     private fun setupManagers() {
@@ -76,6 +120,10 @@ class NewVehicleActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
         binding.toolbar.setNavigationOnClickListener { finish() }
+
+        binding.image.setOnClickListener {
+            onTakePicture()
+        }
 
         binding.latitude.addTextChangedListener { updateMapFromTextFields() }
         binding.longitude.addTextChangedListener { updateMapFromTextFields() }
@@ -130,8 +178,53 @@ class NewVehicleActivity : AppCompatActivity() {
             Toast.makeText(this, R.string.error_validate_form_location, Toast.LENGTH_SHORT).show()
             return false
         }
-        // TODO: Adicionar validação de imagem (se o usuário tirou uma foto).
+        if (uploadedImageUrl.isNullOrBlank()) {
+            Toast.makeText(this, "Por favor, tire uma foto do veículo", Toast.LENGTH_SHORT).show()
+            return false
+        }
         return true
+    }
+
+    private fun onTakePicture() {
+        if (checkSelfPermission(this, android.Manifest.permission.CAMERA) == PERMISSION_GRANTED) {
+            openCamera()
+        } else {
+            requestCameraPermission()
+        }
+    }
+
+    private fun requestCameraPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.CAMERA),
+            REQUEST_CODE_CAMERA
+        )
+    }
+
+    private fun openCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        imageUri = createImageUri()
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+        cameraLauncher.launch(intent)
+    }
+
+    private fun createImageUri(): Uri {
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd-HHmmss").format(Date())
+        val imageFileName = "JPEG_${timeStamp}_"
+
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+
+        imageFile = File.createTempFile(
+            imageFileName,
+            ".jpg",
+            storageDir
+        )
+
+        return FileProvider.getUriForFile(
+            this,
+            "com.example.myapitest.fileprovider",
+            imageFile!!
+        )
     }
 
     private fun createVehicle() {
@@ -150,21 +243,32 @@ class NewVehicleActivity : AppCompatActivity() {
             name = binding.name.text.toString(),
             year = binding.year.text.toString(),
             licence = binding.license.text.toString(),
-            imageUrl = "https://picsum.photos/200", // TODO: Substituir pela URL da imagem capturada
+            imageUrl = uploadedImageUrl!!,
             place = Location(lat = lat, long = long)
         )
+
+        binding.createCTA.isEnabled = false
 
         CoroutineScope(Dispatchers.IO).launch {
             val result = safeApiCall { RetrofitClient.apiService.createVehicle(newVehicle) }
 
             withContext(Dispatchers.Main) {
+                binding.createCTA.isEnabled = true
                 when (result) {
                     is Result.Success -> {
-                        Toast.makeText(this@NewVehicleActivity, R.string.success_create, Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            this@NewVehicleActivity,
+                            "Veículo ${newVehicle.name} criado com sucesso!",
+                            Toast.LENGTH_SHORT
+                        ).show()
                         finish()
                     }
                     is Result.Error -> {
-                        Toast.makeText(this@NewVehicleActivity, R.string.error_create, Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            this@NewVehicleActivity,
+                            "Erro ao criar veículo",
+                            Toast.LENGTH_LONG
+                        ).show()
                     }
                 }
             }
@@ -172,6 +276,7 @@ class NewVehicleActivity : AppCompatActivity() {
     }
 
     companion object {
+        const val REQUEST_CODE_CAMERA = 101
         fun newIntent(context: Context) = Intent(context, NewVehicleActivity::class.java)
     }
 }
